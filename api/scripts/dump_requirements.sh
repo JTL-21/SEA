79c1acd4894461929cc5ccc0fcc83ddbdb0f50b8
JTL-21 <jamestlondon2021@gmail.com>
2023-09-02 19:09:21 +0000

    add makefile

--- api/scripts/dump_requirements.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/bash
-
-pip freeze > requirements.txt
-echo Dumped requirements from pip into requirements.txt
--- api/scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-
-python3 -m flask --app src --debug run --host=0.0.0.0
