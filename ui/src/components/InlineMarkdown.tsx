b0b04083dc9a06ca25071d3361f8a857170ff54b
JTL-21 <jamestlondon2021@gmail.com>
2023-09-02 19:09:29 +0000

    add inline markdown

---
+++ ui/src/components/InlineMarkdown.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import cn from "clsx";
+
+interface Props
+  extends Omit<React.ComponentPropsWithoutRef<"div">, "children"> {
+  children?: string | string[];
+}
+
+const allowedElements = ["b", "i", "em", "p", "a", "pre", "code", "strong"];
+
+const Markdown = ({ children = "", className, ...divProps }: Props) => {
+  return (
+    <ReactMarkdown
+      className={cn("prose-sm prose-p:mt-0", className)}
+      allowedElements={allowedElements}
+      unwrapDisallowed={true}
+      {...divProps}
+    >
+      {typeof children === "object" ? children.join(" ") : children}
+    </ReactMarkdown>
+  );
+};
+
+export default Markdown;
