534e023513f2d0b82e58bb28daaee3a556f2a18a
JTL-21 <jamestlondon2021@gmail.com>
2023-09-02 19:10:11 +0000

    add comment model tests

---
+++ api/tests/models/test_comment_model.py
@@ -0,0 +1,11 @@
+import pytest
+from app.models.Comment import Comment
+
+
+@pytest.mark.usefixtures("load_mock_data")
+def test_as_dict():
+    comment = Comment.query.first()
+
+    comment_dict = comment.as_dict()
+
+    assert type(comment_dict) == dict
