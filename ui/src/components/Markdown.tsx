4bbfa88b33d74bd44f4d12d6388c892c3b5d5acc
JTL-21 <jamestlondon2021@gmail.com>
2023-09-02 19:09:47 +0000

    change markdown margins

--- ui/src/components/Markdown.tsx
+++ ui/src/components/Markdown.tsx
@@ -11,7 +11,7 @@ const Markdown = ({ children, className, ...divProps }: Props) => {
   return (
     <ReactMarkdown
       className={cn(
-        "prose prose-headings:m-0 prose-headings:font-semibold prose-ul:m-0 prose-ol:m-0 prose-p:mt-0 prose-hr:my-2 prose-blockquote:my-2",
+        "prose prose-headings:m-0 prose-li:my-0.5 prose-headings:font-semibold prose-headings:mt-4 prose-ul:m-0 prose-ol:m-0 prose-p:mt-0 prose-hr:my-2 prose-blockquote:my-2",
         className
       )}
       {...divProps}
