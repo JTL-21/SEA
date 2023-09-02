8f5e6c6798b3caffbb6f360d628832d2d86c67c8
JTL-21 <jamestlondon2021@gmail.com>
2023-09-02 19:09:52 +0000

    add navbar to topbar

--- ui/src/components/TopBar.tsx
+++ ui/src/components/TopBar.tsx
@@ -3,20 +3,28 @@ import { Link } from "react-router-dom";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
 import useUser from "../hooks/useUser";
 
+const topBarLinkClasses = "text-gray-500 hover:underline";
+
 const TopBar = () => {
   const { user } = useUser();
 
   return (
     <div
-      className="fixed left-0 right-0 top-0 h-14 bg-white p-3 shadow"
+      className="fixed left-0 right-0 top-0 h-14 bg-white p-4 shadow"
       title="home"
     >
-      <div className="mx-auto flex h-full items-center gap-2">
-        <Link to="/" className="flex h-full items-center gap-2">
-          <img src={logo} alt="logo" className="h-full" />
-          <h1 className="select-none text-3xl font-light text-amber-950">
+      <div className="flex h-full items-center gap-2">
+        <Link to="/" className="mr-2 flex items-center gap-2">
+          <img src={logo} alt="logo" className="block h-6" />
+          <h1 className="-mt-1 select-none text-2xl font-light text-amber-950">
             Kong
           </h1>
+        </Link>
+        <Link to="/projects" className={topBarLinkClasses}>
+          Projects
+        </Link>
+        <Link to="/create-project" className={topBarLinkClasses}>
+          Create Project
         </Link>
         <div className="flex-grow"></div>
         <div
